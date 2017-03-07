ca732858b6b5d650e3c02b8c74a52c540c738534
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-03-07 07:44:18 +0100

    string length for utf8 code points

--- lib/include/tweeteria/string_util.hpp
+++ lib/include/tweeteria/string_util.hpp
@@ -8,6 +8,48 @@ namespace tweeteria
 {
 std::wstring convertUtf8ToUtf16(std::string const& utf8_str);
 std::string convertUtf16ToUtf8(std::wstring const& utf16_str);
+std::size_t lengthUtf8CodePoints(char const* utf8_str);
+std::size_t lengthUtf8CodePoints(char const* utf8_str, std::size_t length_code_units);
+std::size_t lengthUtf8CodePoints(std::string const& utf8_str);
+
+template<class ForwardIt>
+inline std::size_t lengthUtf8CodePoints(ForwardIt first, ForwardIt last)
+{
+    auto it = first;
+    std::size_t count = 0;
+    while(it != last) {
+        if(((*it) & 0x80) == 0x00) {
+            // single byte code point
+            ++count;
+            ++it;
+        } else if(((*it) & 0xE0) == 0xC0) {
+            // two byte code point
+            if(((*(++it)) & 0xC0) != 0x80) { return 0xFFFFFFFF; }
+            ++count;
+            ++it;
+        } else if(((*it) & 0xF0) == 0xE0) {
+            // three byte code point
+            if(((*(++it)) & 0xC0) != 0x80) { return 0xFFFFFFFF; }
+            if(((*(++it)) & 0xC0) != 0x80) { return 0xFFFFFFFF; }
+            ++count;
+            ++it;
+        } else if(((*it) & 0xF8) == 0xF0) {
+            // four byte code point
+            if(((*(++it)) & 0xC0) != 0x80) { return 0xFFFFFFFF; }
+            if(((*(++it)) & 0xC0) != 0x80) { return 0xFFFFFFFF; }
+            if(((*(++it)) & 0xC0) != 0x80) { return 0xFFFFFFFF; }
+            ++count;
+            ++it;
+        } else if(((*it) & 0xC0) == 0x80) {
+            // middle of a code point
+            return 0xFFFFFFFF;
+        } else {
+            // invalid code unit
+            return 0xFFFFFFFF;
+        }
+    }
+    return count;
+}
 }
 
 #endif
--- lib/src/string_util.cpp
+++ lib/src/string_util.cpp
@@ -1,5 +1,5 @@
 
-#include <tweeteria/user.hpp>
+#include <tweeteria/string_util.hpp>
 
 #include <codecvt>
 #include <locale>
@@ -16,5 +16,48 @@ std::string convertUtf16ToUtf8(std::wstring const& utf16_str)
 {
     std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
     return converter.to_bytes(utf16_str);
+}
+
+namespace {
+class ZeroTermIterator
+{
+    friend bool operator!=(ZeroTermIterator lhs, ZeroTermIterator rhs);
+private:
+    char const* it_;
+public:
+    typedef typename std::iterator_traits<char const*>::difference_type difference_type;
+    ZeroTermIterator()
+        :it_(nullptr)
+    {}
+    ZeroTermIterator(char const* it)
+        :it_(it)
+    {}
+    ZeroTermIterator operator++() {
+        return ZeroTermIterator(++it_);
+    }
+    char operator*() {
+        return *it_;
+    }
+};
+
+bool operator!=(ZeroTermIterator lhs, ZeroTermIterator /*rhs*/)
+{
+    return (*lhs.it_) != '\0';
+}
+}
+
+std::size_t lengthUtf8CodePoints(char const* utf8_str)
+{
+    return lengthUtf8CodePoints(ZeroTermIterator{utf8_str}, ZeroTermIterator{});
+}
+
+std::size_t lengthUtf8CodePoints(char const* utf8_str, std::size_t length_code_units)
+{
+    return lengthUtf8CodePoints(utf8_str, utf8_str + length_code_units);
+}
+
+std::size_t lengthUtf8CodePoints(std::string const& utf8_str)
+{
+    return lengthUtf8CodePoints(begin(utf8_str), end(utf8_str));
 }
 }
