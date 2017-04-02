289267050b10639476e8b0283c8d2c171a822f09
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-04-02 16:34:21 +0200

    added forward decl header to lib

---
+++ lib/include/tweeteria/forward_decl.hpp
@@ -0,0 +1,22 @@
+
+#ifndef TWEETERIA_INCLUDE_GUARD_FORWARD_DECL_HPP
+#define TWEETERIA_INCLUDE_GUARD_FORWARD_DECL_HPP
+
+namespace tweeteria
+{
+struct Cursor;
+struct Entities;
+struct Error;
+struct Errors;
+template<typename T> class MultiPageResult;
+struct OAuthCredentials;
+class ProfileImageUrl;
+struct ProxyConfig;
+struct Tweet;
+class Tweeteria;
+struct User;
+struct UserEntities;
+struct VerificationResult;
+}
+
+#endif
